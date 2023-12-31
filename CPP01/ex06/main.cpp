5211415eae64cc63b9fc01860ba4343bf650de36
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-12-31 18:25:02 +0900

    push

---
+++ CPP01/ex06/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/31 17:55:53 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/12/31 18:19:59 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void)
+{
+	std::string input;
+	Harl harl;
+	std::cout << "Hello, world!" << std::endl;
+
+	do
+	{
+		std::cout << "Enter a command {DEBUG/INFO/WARNING/ERROR}: ";
+		std::cin >> input;
+		harl.complain(input);
+	} while (1);
+	return (0);
+}
