bedcc0558ea534706278fdd89b26f18a09700dd2
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2023-12-31 11:56:58 +0900

    push

--- CPP01/ex03/Weapon.cpp
+++ CPP01/ex03/Weapon.cpp
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/17 18:59:44 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/12/17 19:07:19 by yokitaga         ###   ########.fr       */
+/*   Created: 2023/12/17 18:59:41 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/12/17 18:59:41 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WEAPON_HPP
-# define WEAPON_HPP
-
-class Weapon
-{
-private:
-	std::string	_type;
-public:
-	Weapon(std::string type);
-	~Weapon();
-	const std::string&	getType();
-	void				setType(std::string type);
-};
-
-#endif
---
+++ CPP01/ex03/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 18:59:54 by yokitaga          #+#    #+#             */
+/*   Updated: 2023/12/17 19:01:17 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
