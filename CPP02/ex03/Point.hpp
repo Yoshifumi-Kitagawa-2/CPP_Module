106b91a703ff751fb83f64917d1f2cea38bdcdc4
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-01-03 00:16:44 +0900

    push

---
+++ CPP02/ex03/Point.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 00:03:36 by yokitaga          #+#    #+#             */
+/*   Updated: 2024/01/03 00:13:13 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef POINT_HPP
+# define POINT_HPP
+
+# include "Fixed.hpp"
+
+class Point
+{
+	private:
+		Fixed const _x;
+		Fixed const _y;
+	public:
+		Point();
+		Point(const float x, const float y);
+		Point(const Point &src);
+		Point &operator=(const Point &right);
+		~Point();
+		Fixed getX() const;
+		Fixed getY() const;
+		bool isSame(const Point &point) const;
+};
+
+
+#endif
