feb99b0bd43a58497527c7bf2751054c6e06f20d
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-01-03 00:20:31 +0900

    push

--- CPP02/ex03/bsq.cpp
+++ CPP02/ex03/bsq.cpp
@@ -6,9 +6,21 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 00:04:02 by yokitaga          #+#    #+#             */
-/*   Updated: 2024/01/03 00:15:45 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/01/03 00:19:22 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+bool	bsq(Point const a, Point const b, Point const c, Point const point)
+{
+	if (a.isSame(point) || b.isSame(point) || c.isSame(point))
+		return (false);
+	if ((b.getX() - a.getX()) * (point.getY() - a.getY()) - (b.getY() - a.getY()) * (point.getX() - a.getX()) < 0)
+		return (false);
+	if ((c.getX() - b.getX()) * (point.getY() - b.getY()) - (c.getY() - b.getY()) * (point.getX() - b.getX()) < 0)
+		return (false);
+	if ((a.getX() - c.getX()) * (point.getY() - c.getY()) - (a.getY() - c.getY()) * (point.getX() - c.getX()) < 0)
+		return (false);
+	return (true);
+}
