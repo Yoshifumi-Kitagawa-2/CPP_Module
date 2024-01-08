cb2c6c102bc83ac6249d8c9f51eab3980b6fb47c
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-01-08 22:44:45 +0900

    push

--- CPP04/ex02/AAnimal.cpp
+++ CPP04/ex02/AAnimal.cpp
@@ -6,34 +6,34 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 00:13:34 by yokitaga          #+#    #+#             */
-/*   Updated: 2024/01/08 22:34:11 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/01/08 22:44:02 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AAnimal.hpp"
 
 AAnimal::AAnimal() : _type("AAnimal") {
-	std::cout << "[" << _type << "] constructor called " << std::endl;
+	std::cout << "[" << _type << "] constructor called at AAnimal.cpp" << std::endl;
 }
 
 AAnimal::AAnimal(std::string type) : _type(type) {
-	std::cout << "[" << _type << "] constructor called " << std::endl;
+	std::cout << "[" << _type << "] constructor called at AAnimal.cpp" << std::endl;
 }
 
 AAnimal::AAnimal(const AAnimal &src) {
 	*this = src;
-	std::cout << "[" << _type << "] copy constructor called " << std::endl;
+	std::cout << "[" << _type << "] copy constructor called at AAnimal.cpp" << std::endl;
 }
 
 AAnimal &AAnimal::operator=(const AAnimal &right) {
 	if (this != &right)
 		_type = right._type;
-	std::cout << "[" << _type << "] operator= called " << std::endl;
+	std::cout << "[" << _type << "] operator= called at AAnimal.cpp" << std::endl;
 	return (*this);
 }
 
 AAnimal::~AAnimal() {
-	std::cout << "[AAnimal] destructor called " << std::endl;
+	std::cout << "[" << _type << "] destructor called at AAnimal.cpp" << std::endl;
 }
 
 std::string AAnimal::getType() const {
