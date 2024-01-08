ba46bb12de467f6fe3b8321675651c47b1fe1422
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-01-08 22:39:44 +0900

    push

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 00:13:38 by yokitaga          #+#    #+#             */
-/*   Updated: 2024/01/08 19:02:29 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/01/08 22:39:21 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,29 @@
 int main()
 {
 	//success delete because of virtual destructor
-	std::cout << "==========Test1 : success override and delete because of virtual function and virtual destructor==========" << std::endl;
+	std::cout << "==========Test1 : success overriding and deleting because of virtual function and virtual destructor==========" << std::endl;
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
-	std::cout << "==========success override==========" << std::endl;
+	std::cout << "==========success overriding==========" << std::endl;
 	i->makeSound();
 	j->makeSound();
 	meta->makeSound();
-	std::cout << "==========success delete==========" << std::endl;
+	std::cout << "==========success deleting==========" << std::endl;
 	delete meta;
 	delete j;
 	delete i;
 	//failed delete because of non-virtual destructor
-	std::cout << "==========Test2 : failed override and delete because of non-virtual function and non-virtual destructor==========" << std::endl;
+	std::cout << "==========Test2 : fail overriding and deleting because of non-virtual function and non-virtual destructor==========" << std::endl;
 	const WrongAnimal* wrong_meta = new WrongAnimal();
 	const WrongAnimal* wrong_i = new WrongCat();
 	std::cout << wrong_i->getType() << " " << std::endl;
-	std::cout << "==========failed override==========" << std::endl;
+	std::cout << "==========fail overriding==========" << std::endl;
 	wrong_i->makeSound();
 	wrong_meta->makeSound();
-	std::cout << "==========failed delete==========" << std::endl;
+	std::cout << "==========fail deleting==========" << std::endl;
 	delete wrong_meta;
 	delete wrong_i;
 	return 0;
