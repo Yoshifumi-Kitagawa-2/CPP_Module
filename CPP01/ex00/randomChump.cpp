fc972418a336ed1caea74879aa5952a944cd9732
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-01-21 23:57:42 +0900

    push

--- CPP01/ex00/randomChump.cpp
+++ CPP01/ex00/randomChump.cpp
@@ -6,14 +6,14 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 17:44:35 by yokitaga          #+#    #+#             */
-/*   Updated: 2024/01/06 20:02:58 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/01/21 23:21:13 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void    randomChump( std::string name ) {
-    Zombie  zombi( name );
+    Zombie  zombie( name );
     std::cout << "[in randomChump] " << name << " is created by stack." << std::endl;
-    zombi.announce();
+    zombie.announce();
 }
