319c721b4285821a5d796096f97a20e2630c162d
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-01-22 19:19:41 +0900

    push

--- CPP01/ex03/HumanA.cpp
+++ CPP01/ex03/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 18:55:42 by yokitaga          #+#    #+#             */
-/*   Updated: 2024/01/20 11:21:04 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/01/22 14:08:42 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ HumanA::~HumanA()
 	std::cout << "HumanA " << _name << " is dead" << std::endl;
 }
 
+//このconstはattack関数内でのthisの変更を防ぐ
 void	HumanA::attack(void) const
 {
 	std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
--- CPP01/ex03/Weapon.cpp
+++ CPP01/ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 18:59:41 by yokitaga          #+#    #+#             */
-/*   Updated: 2023/12/31 15:12:22 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/01/22 14:16:53 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ Weapon::~Weapon()
 {
 }
 
+//返り値のconst std::string&は返された文字列への変更を防ぐためのもの
 const std::string&	Weapon::getType(void)
 {
 	return (this->_type);
