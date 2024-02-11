3333c6b719389f31cbbae62ea283f04a85e714f0
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-02-11 10:38:14 +0900

    ex01最低限完成

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 19:03:52 by yokitaga          #+#    #+#             */
-/*   Updated: 2024/02/10 23:20:36 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/02/11 10:37:57 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int main()
 {
-	Serializer s;
 	Data data;
 	data.s1 = "Hello";
 	data.s2 = "World";
-	uintptr_t raw = s.serialize(&data);
-	Data *ptr = s.deserialize(raw);
+	uintptr_t raw = Serializer::serialize(&data);
+	Data *ptr = Serializer::deserialize(raw);
 	std::cout << "s1: " << ptr->s1 << std::endl;
 	std::cout << "s2: " << ptr->s2 << std::endl;
 	return 0;
 }
-
