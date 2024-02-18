a16cff9c08a4cd7d0cd40d0ec39e3703f0701290
Yoshifumi Kitagawa <kitapo3110@gmail.com>
2024-02-18 18:19:25 +0900

    cpp08/ex00最低限

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,13 +6,25 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:03:03 by yokitaga          #+#    #+#             */
-/*   Updated: 2024/02/12 18:59:20 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/02/18 18:15:01 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <algorithm>
 
+template <typename T>
+typename T::iterator easyfind(T& container, int num)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), num);
+	if (it != container.end()) {
+        return it;
+    } else {
+       throw std::out_of_range("Value not found");
+    }
+}
 
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,13 +6,42 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:02:40 by yokitaga          #+#    #+#             */
-/*   Updated: 2024/02/12 18:59:38 by yokitaga         ###   ########.fr       */
+/*   Updated: 2024/02/18 18:19:03 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <stdexcept>
+#include <vector>
 
 int main()
 {
+	std::vector<int> numbers;
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_back(4);
+	numbers.push_back(5);
+
+    try {
+        int target_value = 2;
+
+        std::vector<int>::iterator it = easyfind(numbers, target_value);
+
+        std::cout << "Value:" << target_value << std::endl;
+		std::cout << "index:" << *it << std::endl;
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    try {
+        int target_value = 7;
+
+        std::vector<int>::iterator it = easyfind(numbers, target_value);
+        std::cout << "Value " << target_value << std::endl;
+		std::cout << "index:" << *it << std::endl;
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 	return (0);
 }
